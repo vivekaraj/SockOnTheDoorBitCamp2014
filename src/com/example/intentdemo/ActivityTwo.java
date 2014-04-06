f73ace3b27940863e1bb5b2e51975e11f8596929
baiddhruv <madgooner2810@gmail.com>
2014-04-05 20:01:31 -0400

    fixing button functionality

--- src/com/example/intentdemo/ActivityTwo.java
+++ src/com/example/intentdemo/ActivityTwo.java
@@ -2,6 +2,9 @@ package com.example.intentdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ActivityTwo extends Activity {
@@ -11,10 +14,22 @@ public class ActivityTwo extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity_two);
 		
-		TextView tvText = (TextView) findViewById(R.id.tvText);
 		
-		String text = getIntent().getStringExtra("phoneNumber");
+		String numberholder = getIntent().getStringExtra("phoneNumber");
 		
-		tvText.setText(text);
+		final Button bTurnOn = (Button) findViewById(R.id.bTurnOn);
+		
+		bTurnOn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {				
+				if(bTurnOn.getText().equals("Turn it on")) {
+					bTurnOn.setText("Turn it off");
+				} else {
+					bTurnOn.setText("Turn it on");
+				}
+			}
+		});
+
 	}
 }
--- src/com/example/intentdemo/MainActivity.java
+++ src/com/example/intentdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.intentdemo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,21 +14,36 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		final SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+		
+		final EditText etPhoneNumber = (EditText) findViewById(R.id.etPhoneNumber);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		final EditText etPhoneNumber = (EditText) findViewById(R.id.etPhoneNumber);
+		
 		Button bOkay = (Button) findViewById(R.id.bOkay);
 		
 		bOkay.setOnClickListener(new OnClickListener() {
+			
 			
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(MainActivity.this, ActivityTwo.class);
 				String text = etPhoneNumber.getText().toString();
+				try {
+					int phNumber = Integer.parseInt(text);
+				} catch(Exception e) {
+					
+				}
 				i.putExtra("phoneNumber", text);
+				SharedPreferences.Editor editor = sharedPref.edit();
+				editor.putInt(getString(R.string.saved_high_score), newHighScore);
+				editor.commit();
 				startActivity(i);
 			}
 		});
+		
+		
 	}
 }
+ 
